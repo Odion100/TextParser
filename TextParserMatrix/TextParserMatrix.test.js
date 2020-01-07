16bba2fcef8b90a7eafca23087d17e1a09510d51
Odion100 <odionedwards@gmail.com>
2020-01-07 15:37:26 -0500

    renamed folder and added intial test setup

---
+++ TextParserMatrix/TextParserMatrix.test.js
@@ -0,0 +1,19 @@
+const chai = require("chai");
+const { expect } = chai;
+
+describe("TextParserMatrix Test", () => {
+  it("should create and return an object with the following properties and methods:table, addText, sort, print", () => {
+    //
+  });
+  it("should add rows of delimited text to the table", () => {
+    //
+  });
+
+  it("should be able to exclude a column of delimited text", () => {
+    //
+  });
+
+  it("should be able to map delimited text to specific columns", () => {
+    //
+  });
+});
