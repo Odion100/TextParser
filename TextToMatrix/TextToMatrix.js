108c190f74d4682642ff9f1b7007bd41790c73e8
Odion100 <odionedwards@gmail.com>
2020-01-07 10:48:32 -0500

    project initial setup

---
+++ TextToMatrix/TextToMatrix.js
@@ -0,0 +1,19 @@
+//Creates a table of
+const TextToMatrix = () => {
+  const table = [];
+
+  const addText = ({ text = [], delimiter, excludeRow }) => {
+    //split by newline
+    const newRows = text.split(/\r?\n/);
+    //split by delimeter
+    //insert new row in table
+  };
+
+  const sort = sortIndex => {};
+
+  return {
+    table,
+    addText,
+    sort
+  };
+};
